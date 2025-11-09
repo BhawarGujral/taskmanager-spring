1b91ffa942a3c7147a9cd133697811f147f074d9
Bhawar Gujral <Bhawargujral@gmail.com>
2025-11-09 18:36:31 -0500

    Implement the Update Task Status(Green Phase)

--- src/main/java/com/bhawar/taskmanager/service/TaskService.java
+++ src/main/java/com/bhawar/taskmanager/service/TaskService.java
@@ -1,10 +1,28 @@
 package com.bhawar.taskmanager.service;
 
 import com.bhawar.taskmanager.model.Task;
+import com.bhawar.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class TaskService {
 
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     public Task updateTaskStatus(Long id, String status){
+        Optional<Task> task = taskRepository.findById(id);
+        if(task.isPresent()){
+            Task t = task.get();
+            t.setStatus(status);
+            return taskRepository.save(t);
+        }
         return null;
     }
 
