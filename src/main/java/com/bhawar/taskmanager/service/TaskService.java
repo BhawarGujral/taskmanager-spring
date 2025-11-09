9a85037c67b76ed9b697ebdc80118ccc8f79c2bc
Bhawar Gujral <Bhawargujral@gmail.com>
2025-11-09 18:34:00 -0500

    Implement 1st test using Mockito (red phase)

--- src/main/java/com/bhawar/taskmanager/service/TaskService.java
+++ src/main/java/com/bhawar/taskmanager/service/TaskService.java
@@ -1,4 +1,11 @@
 package com.bhawar.taskmanager.service;
 
+import com.bhawar.taskmanager.model.Task;
+
 public class TaskService {
+
+    public Task updateTaskStatus(Long id, String status){
+        return null;
+    }
+
 }
--- src/test/java/com/bhawar/taskmanager/service/TaskServiceTest.java
+++ src/test/java/com/bhawar/taskmanager/service/TaskServiceTest.java
@@ -1,19 +1,26 @@
 package com.bhawar.taskmanager.service;
 
+import com.bhawar.taskmanager.model.Task;
 import com.bhawar.taskmanager.repository.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 //Modern way of telling spring to initial mock class whenever we run this class
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
 
     //We are creating mock of the repository not actually intializing it because
-    // we dont want to touch real database data
+    // we dont want to touch real database data and database interactions are costly to run
     @Mock
     private TaskRepository taskRepository;
 
@@ -27,5 +34,26 @@ public class TaskServiceTest {
 //        MockitoAnnotations.openMocks(this);
 //    }
 
-    
+    @Test
+    void testUpdateTaskStatus(){
+        //aarange
+        //ID will not be auto generated because its a mock
+        Task task = new Task(1L,"Existing Task","to do");
+
+        // Find by id definition for dummy
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+        //Save definition for dummy
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
+
+        //act
+        Task updatedTask = taskService.updateTaskStatus(1L,"In Progress");
+
+        //assert
+        assertNotNull(updatedTask);
+        assertEquals("In Progress", updatedTask.getStatus());
+        //To make sure dummy is being called
+        verify(taskRepository,times(1)).findById(1L);
+        verify(taskRepository,times(1)).save(task);
+    }
+
 }
