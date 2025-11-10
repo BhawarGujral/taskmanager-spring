701b43a4a3f3cc825c2e4edbd35efc73157670a6
Bhawar Gujral <Bhawargujral@gmail.com>
2025-11-09 21:51:41 -0500

    Added implementation and Refactored it as well.

--- src/main/java/com/bhawar/taskmanager/controller/TaskController.java
+++ src/main/java/com/bhawar/taskmanager/controller/TaskController.java
@@ -1,4 +1,26 @@
 package com.bhawar.taskmanager.controller;
 
+import com.bhawar.taskmanager.model.Task;
+import com.bhawar.taskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskService.getAllTasks();
+    }
 }
