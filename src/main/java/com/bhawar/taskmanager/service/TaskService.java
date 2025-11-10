7525c9d3a427b8c2288723ae25df3bd223189a76
Bhawar Gujral <Bhawargujral@gmail.com>
2025-11-09 21:20:42 -0500

    Implement and add all the test cases for CRUD operations/

--- src/main/java/com/bhawar/taskmanager/service/TaskService.java
+++ src/main/java/com/bhawar/taskmanager/service/TaskService.java
@@ -1,10 +1,12 @@
 package com.bhawar.taskmanager.service;
 
+import com.bhawar.taskmanager.exception.TaskNotFoundException;
 import com.bhawar.taskmanager.model.Task;
 import com.bhawar.taskmanager.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,16 +18,34 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+
     public Task updateTaskStatus(Long id, String status){
         Task taskToUpdate = getTaskById(id);
-        if(taskToUpdate == null){
-            return null;
-        }
         taskToUpdate.setStatus(status);
         return taskRepository.save(taskToUpdate);
     }
 
+    public Task updateTask(Long id,Task updatedTaskData){
+        Task taskToUpdate = getTaskById(id);
+        taskToUpdate.setTitle(updatedTaskData.getTitle());
+        taskToUpdate.setStatus(updatedTaskData.getStatus());
+        return taskRepository.save(taskToUpdate);
+    }
+
     public Task getTaskById(Long id){
-        return taskRepository.findById(id).orElse(null);
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found , id: " + id));
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public void deleteTask(Long id){
+        Task taskToDelete = getTaskById(id);
+        taskRepository.delete(taskToDelete);
     }
 }
